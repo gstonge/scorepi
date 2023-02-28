eaaf371369817594ac7bc11fe7150d625657e8b2
abbyokleung <abbyokleung@gmail.com>
2023-02-28 10:41:30 -0500

    comments added, change 'target_end_date' to pred.t_col

--- scorepi/score_utils.py
+++ scorepi/score_utils.py
@@ -207,7 +207,11 @@ def all_scores_from_df(observations, predictions, interval_ranges=[10,20,30,40,5
 def intersec(predictions,observations):
         pred = predictions.copy()
         obs = observations.copy()
-        pred = pred[pred.groupby("target_end_date").type.transform(lambda x: x.nunique()).ge(2)]
+        
+        # groups the predictions by pred.t_col,
+        # keeping the date if there are more than or equal to 2 unique prediction types 
+        # ("point" and "quantile")
+        pred = pred[pred.groupby(pred.t_col).type.transform(lambda x: x.nunique()).ge(2)]
 
         for col in observations.ind_cols:
             pred = pred[pred[col].isin(obs[col])]
